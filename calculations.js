5b0da787981cc0a9f2b3f7bdf91f87487918c84a
kyleghousel <69773129+kyleghousel@users.noreply.github.com>
2022-12-28 08:55:58 -0500

    Calculator basic level of functional
    Keys work with clicks only, can't seem to enter numbers with more than one digit, operators work though. Working through those and edge cases.

--- calculations.js
+++ calculations.js
@@ -1,3 +1,69 @@
+const calculator = document.querySelector('.calculator');
+const keys = calculator.querySelector('.calculatorKeys');
+const display = document.querySelector('.calculator-screen');
+
+keys.addEventListener('click', e => {
+    const key = e.target;
+    const action = key.dataset.action;
+    if (e.target.matches('button')) {
+        const key = e.target;
+        const action = key.dataset.action;
+        const keyContent = key.textContent;
+        const displayedNum = display.textContent;
+        const previousKeyType = calculator.dataset.previousKeyType;
+   
+    if (!action) {
+        if (displayedNum === '0' || previousKeyType === 'operator') {
+            display.textContent = keyContent;
+        } else {
+            display.textContent = displayedNum + keyContent;
+        }
+    }
+    if (
+        action === 'add' ||
+        action === 'subtract' ||
+        action === 'multiply' ||
+        action === 'divide'
+
+    ) {
+        calculator.dataset.previousKeyType = 'operator';
+        calculator.dataset.firstValue = displayedNum;
+        calculator.dataset.operator = action;
+    }
+    if (action === 'decimal') {
+        display.textContent = displayedNum + '.'
+    }
+    if (action === 'all-clear') {
+        display.textContent = 0;
+    }
+    if (action === 'calculate') {
+        const firstValue = calculator.dataset.firstValue;
+        const operator = calculator.dataset.operator;
+        const secondValue = displayedNum;
+
+        display.textContent = operate(operator, firstValue, secondValue)
+    }
+   }
+
+});
+
+
+function clearAll () {
+    clear.addEventListener('click', (e) => {
+        screen.innerText = '0';
+    })
+}
+
+
+document.addEventListener('click', function onClick(e) {
+    if (e.keyCode === 49) {
+        screen.innerText = '1';
+    }
+    console.log(e);
+
+});
+
+
 function add (a, b) {
     return a + b;
 }
@@ -15,25 +81,19 @@ function divide (a, b) {
     return a / b;
 }
 
-function power (a, b) {
-	return Math.pow(a, b);
-};
-
 function operate(operator, a, b) {
-    a = Number(a)
-    b = Number(b)
+    a = parseFloat(a)
+    b = parseFloat(b)
     switch (operator) {
-      case '+':
+      case 'add':
         return add(a, b)
-      case '−':
+      case 'subtract':
         return subtract(a, b)
-      case '×':
+      case 'multiply':
         return multiply(a, b)
-      case '÷':
+      case 'divide':
         if (b === 0) return null
         else return divide(a, b)
-      case '^':
-        return power (a, b)
       default:
         return null
     }
@@ -42,3 +102,17 @@ function operate(operator, a, b) {
 /*const sum = function(array) {
 	return array.reduce((partialSum, a) => partialSum + a, 0);
 };*/
+
+
+/*
+1. Capture input and display on calculator screen
+    a. Do so with a function. Store the display
+    value in a variable to be used in order to 
+    use the operations.
+2. Clear button sets display back to 0
+3. Store first input value when user selects operator
+4. Operate should then use function used to populate the display
+5. Figure out how to string together operations
+6. Throw error on calculator display if '=' is pressed
+prior to all numbers being entered or an operator 
+*/
